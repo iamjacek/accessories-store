313f1715be16f96b3a7a9d3f62008cf7e9ee358b
iamjacek <iamjacek@icloud.com>
2020-11-26 18:54:15 +0000

    footer logo and styling

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,37 +1,40 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import line from '../../assets/logo-line.svg'
 
 const Footer = () => (
-<footer id="footer" className="bg-gray-700">
+<footer id="footer" className="bg-white mt-2">
 
 {/* <!-- start container --> */}
 <div className="container mx-auto px-12 py-8 sm:py-6">
 
-    <div className="flex flex-wrap overflow-hidden sm:-mx-1 md:-mx-px lg:-mx-2 xl:-mx-2">
+    <div className="flex flex-wrap overflow-hidden sm:-mx-1 md:-mx-px lg:-mx-2 xl:-mx-2 pb-4">
 
         <div className="w-full overflow-hidden flex justify-center sm:my-1 sm:px-1 sm:w-1/2 md:my-px md:px-px md:w-1/2 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4 pb-6">
             {/* <!-- Column 1 Content --> */}
             
-            <NavLink to="/" className="flex items-center flex-shrink-0 text-white mr-6">
-    <svg className="fill-current h-10 w-9" width="47" height="47" viewBox="0 0 35 35" xmlns="http://www.w3.org/2000/svg"><path d="M22.507,0H9.175C7.9,0,6.87,1.034,6.87,2.309v27.07c0,1.271,1.03,2.306,2.305,2.306h13.332
-		c1.273,0,2.307-1.034,2.307-2.306V2.309C24.813,1.034,23.78,0,22.507,0z M23.085,25.672H8.599V3.895h14.486V25.672z M18.932,2.343
-		h-6.181V1.669h6.182L18.932,2.343L18.932,2.343z M21.577,2.035c0,0.326-0.266,0.59-0.591,0.59c-0.326,0-0.591-0.265-0.591-0.59
-		s0.265-0.59,0.591-0.59C21.312,1.444,21.577,1.709,21.577,2.035z M18.655,29.225h-5.629v-1.732h5.629V29.225z"/></svg>
-    <span className="font-semibold text-xl tracking-tight">Accessories</span>
-  </NavLink>
+            <NavLink to="/" className="flex flex-col items-center flex-shrink-0 text-white mr-6 -mt-10 -mb-1">
+        <img src={line} alt="logo line" className="transform translate-y-10 pb-1 translate-x-3 shadow-sm"/>
+        <div className="flex flex-row">
+          <span className="font-racing text-5xl tracking-wide text-gray-700 text-shadow-md">beep</span>
+          <span className="font-racing text-5xl tracking-wide text-purple-600 text-shadow">line</span>
+        </div>
+        
+        <p className="text-sm text-gray-600 text-shadow-md transform -translate-y-2 tracking-wide">Selected phone accessories</p>
+      </NavLink>
         </div>
 
         <div className="w-full overflow-hidden flex flex-col justify-center sm:my-1 sm:px-1 sm:w-1/2 md:my-px md:px-px md:w-1/2 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4 pb-6">
             {/* <!-- Column 2 Content --> */}
 
 
-            <h4 className="text-white text-center mb-2">Important</h4>
+            <h4 className="text-white text-center mb-2 text-gray-700">Important</h4>
             <ul className="nav navbar-nav text-center">
                 <li id="navi-2" className="leading-7 text-sm">
-                    <a className="text-gray-500 hover:text-orange-500 text-small" href="/page-1">
+                    <a className="text-gray-600 hover:text-purple-600 text-small" href="/page-1">
                         Page 1 </a>
                 </li>
-                <li id="navi-1" className="leading-7 text-sm"><a className="text-gray-500 hover:text-orange-500 text-small" href="/page-2">Page 2</a></li>
+                <li id="navi-1" className="leading-7 text-sm"><a className="text-gray-600 hover:text-purple-600 text-small" href="/page-2">Page 2</a></li>
             </ul>
 
 
@@ -39,26 +42,26 @@ const Footer = () => (
 
         <div className="w-full overflow-hidden flex flex-col justify-center sm:my-1 sm:px-1 sm:w-1/2 md:my-px md:px-px md:w-1/2 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4 pb-6">
             {/* <!-- Column 3 Content --> */}
-            <h4 className="text-white text-center mb-2">Info</h4>
+            <h4 className="text-white text-center mb-2 text-gray-700">Info</h4>
             <ul className="text-center">
             <li id="navi-2" className="leading-7 text-sm">
-                <a className="text-gray-500 hover:text-orange-500 text-small" href="/page-1">
+                <a className="text-gray-600 hover:text-purple-600 text-small" href="/page-1">
                     Page 1 </a>
             </li>
-            <li id="navi-1" className="leading-7 text-sm"><a className="text-gray-500 hover:text-orange-500 text-small" href="/page-2">Page 2</a></li>
+            <li id="navi-1" className="leading-7 text-sm"><a className="text-gray-600 hover:text-purple-600 text-small" href="/page-2">Page 2</a></li>
             </ul>
         </div>
 
         <div className="w-full overflow-hidden flex flex-col justify-center sm:my-1 sm:px-1 sm:w-1/2 md:my-px md:px-px md:w-1/2 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4 pb-6">
             {/* <!-- Column 4 Content --> */}
 
-            <h4 className="text-white text-center mb-2">Products</h4>
+            <h4 className="text-white text-center mb-2 text-gray-700">Products</h4>
             <ul className="text-center">
             <li id="navi-2" className="leading-7 text-sm">
-                <a className="text-gray-500 hover:text-orange-500 text-small" href="/page-1">
+                <a className="text-gray-600 hover:text-purple-600 text-small" href="/page-1">
                     Page 1 </a>
             </li>
-            <li id="navi-1" className="leading-7 text-sm"><a className="text-gray-500 hover:text-orange-500 text-small" href="/page-2">Page 2</a></li>
+            <li id="navi-1" className="leading-7 text-sm"><a className="text-gray-600 hover:text-purple-600 text-small" href="/page-2">Page 2</a></li>
             </ul>
         </div>
 
@@ -68,11 +71,11 @@ const Footer = () => (
 
     {/* <!-- Start footer bottom --> */}
 
-    <div className="pt-4 flex justify-center md:flex items-center" style={{borderTop:"1px solid white"}}>
-        <ul className="">
-            <li className="sm:mx-2 sm:inline leading-7 text-sm" id="footer-navi-2"><a className="text-white  text-small hover:text-orange-500" href="/disclaimer">Disclaimer</a></li>
-            <li className="sm:mx-2 sm:inline leading-7 text-sm" id="footer-navi-2"><a className="text-white  text-small hover:text-orange-500" href="/cookie">Cookie policy</a></li>
-            <li className="sm:mx-2 sm:inline leading-7 text-sm" id="footer-navi-2"><a className="text-white  text-small hover:text-orange-500" href="/privacy">Privacy</a></li>
+    <div className="pt-4 flex justify-center md:flex items-center border-t-2 border-gray-300">
+        <ul className="text-center">
+            <li className="sm:mx-2 sm:inline leading-7 text-sm" id="footer-navi-2"><a className="text-gray-700  text-small hover:text-purple-600" href="/disclaimer">Disclaimer</a></li>
+            <li className="sm:mx-2 sm:inline leading-7 text-sm" id="footer-navi-2"><a className="text-gray-700  text-small hover:text-purple-600" href="/cookie">Cookie policy</a></li>
+            <li className="sm:mx-2 sm:inline leading-7 text-sm" id="footer-navi-2"><a className="text-gray-700  text-small hover:text-purple-600" href="/privacy">Privacy</a></li>
             </ul>
     </div>
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -38,15 +38,15 @@ const AuthNav = ({ handleSignout }) => {
 
   return (
     <nav className="bg-white pb-2 px-12">
-      <div className="container mx-auto flex items-center flex-wrap justify-between">
-      <NavLink to="/" className="flex flex-col items-center flex-shrink-0 text-white mr-6 -mt-8">
-        <img src={line} alt="logo line" className="transform translate-y-10 pb-1 translate-x-3"/>
+      <div className="container mx-auto flex items-end flex-wrap justify-between">
+      <NavLink to="/" className="flex flex-col items-center flex-shrink-0 text-white mr-6 -mt-10 -mb-1">
+        <img src={line} alt="logo line" className="transform translate-y-10 pb-1 translate-x-3 shadow-sm"/>
         <div className="flex flex-row">
           <span className="font-racing text-5xl tracking-wide text-gray-700 text-shadow-md">beep</span>
-          <span className="font-racing text-5xl tracking-wide text-purple-600 text-shadow-md">line</span>
+          <span className="font-racing text-5xl tracking-wide text-purple-600 text-shadow">line</span>
         </div>
         
-        <p className="text-sm text-gray-600 text-shadow-md transform -translate-y-2">Selected phone accessories</p>
+        <p className="text-sm text-gray-600 text-shadow-md transform -translate-y-2 tracking-wide">Selected phone accessories</p>
       </NavLink>
       <div className="block sm:hidden">
         <button onClick={toggleMobileMenu} className="flex items-center px-3 py-2 border rounded text-orange-100 border-orange-100 hover:text-white hover:border-white">
@@ -62,7 +62,7 @@ const AuthNav = ({ handleSignout }) => {
           }
         </button>
       </div>
-      <div className="w-full hidden sm:block flex-grow sm:flex sm:items-center sm:w-auto sm:pl-1">
+      <div className="w-full hidden sm:block flex-grow sm:flex sm:items-center sm:w-auto sm:pl-1 pb-6">
         <div className="sm:flex-grow">
           <NavLink to='/' exact activeClassName="italic" className="block text-md font-medium sm:inline-block text-gray-700 hover:text-purple-600 mr-4">
             Categories
@@ -70,13 +70,13 @@ const AuthNav = ({ handleSignout }) => {
           
         </div>
         <div className="flex flex-row justify-center items-center">
-          <button className="px-6 py-1 mx-2" >
+          <button className="px-6 py-1 mx-2">
             <img src={basketIcon} alt="basket-trolley" className="h-6"></img>
           </button>
-            <NavLink to='/checkout' activeClassName="text-orange-500" className="block text-md mt-4 sm:inline-block sm:mt-0 text-orange-100 hover:text-orange-500 mr-4 active:text-xl">
+            <NavLink to='/checkout' activeClassName="italic" className="block text-md mt-4 sm:inline-block sm:mt-0 text-gray-700 hover:text-purple-600 mr-4 active:text-xl">
               Checkout
             </NavLink>
-            <button onClick={handleSignout}  className="button__hover inline-block mt-5 text-md px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-700 hover:bg-orange-500 mt-4 sm:mt-0">Sign out</button>
+            <button onClick={handleSignout}  className="bg-purple-600 inline-block mt-5 text-md px-8 py-2 leading-none border rounded-full text-white hover:bg-purple-500 mt-4 sm:mt-0 shadow">Sign out</button>
         </div>
       </div>
       <MobileMenu isOpen={isMenuOpen} toggleX={toggleMobileMenu}/>
@@ -97,9 +97,14 @@ const UnAuthNav = () => {
   return (
     <nav className="bg-white py-4 px-12">
       <div className="container mx-auto flex items-center flex-wrap justify-between">
-      <NavLink to="/" className="flex items-center flex-shrink-0 text-white mr-6 pt-1">
-      <span className="font-racing text-4xl tracking-wide text-gray-700">beep</span>
-        <span className="font-racing text-4xl tracking-wide text-purple-600">line</span>
+      <NavLink to="/" className="flex flex-col items-center flex-shrink-0 text-white mr-6 -mt-10 -mb-2">
+        <img src={line} alt="logo line" className="transform translate-y-10 pb-1 translate-x-3 shadow-sm"/>
+        <div className="flex flex-row">
+          <span className="font-racing text-5xl tracking-wide text-gray-700 text-shadow-md">beep</span>
+          <span className="font-racing text-5xl tracking-wide text-purple-600 text-shadow">line</span>
+        </div>
+        
+        <p className="text-sm text-gray-600 text-shadow-md transform -translate-y-2 tracking-wide">Selected phone accessories</p>
       </NavLink>
       <div className="block sm:hidden">
         <button onClick={toggleMobileMenu} className="flex items-center px-3 py-2 border rounded text-orange-100 border-orange-100 hover:text-white hover:border-white">
@@ -124,10 +129,10 @@ const UnAuthNav = () => {
           <button className="px-6 py-1 mx-2" >
             <img src={basketIcon} alt="basket-trolley" className="h-6"></img>
           </button>
-            <NavLink to='/signin' activeClassName="text-orange-500" className="block text-md mt-4 sm:inline-block sm:mt-0 text-orange-100 hover:text-orange-500 mr-4 active:text-xl">
+            <NavLink to='/signin' activeClassName="text-orange-500" className="block text-md mt-4 sm:inline-block sm:mt-0 text-gray-700 hover:text-purple-600 mr-4 active:text-xl">
               Sign in
             </NavLink>
-            <NavLink to='/signup' activeClassName="text-orange-500 border-orange-500" className="inline-block mt-5 text-md px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-700 hover:bg-orange-500 mt-4 sm:mt-0">Sign up</NavLink>
+            <NavLink to='/signup' className="bg-purple-600 inline-block mt-5 text-md px-8 py-2 leading-none border rounded-full text-white hover:bg-purple-500 mt-4 sm:mt-0">Sign up</NavLink>
         </div>
       </div>
       <MobileMenu isOpen={isMenuOpen} toggleX={toggleMobileMenu}/>
